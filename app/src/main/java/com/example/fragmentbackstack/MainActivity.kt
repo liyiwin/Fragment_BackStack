4f9ff356297d4df7a85725b6546084deba01c467
goodideas <goodideas@goodideasde-MacBook-Pro.local>
2020-05-06 15:10:27 +0800

    All Main function added

--- app/src/main/java/com/example/fragmentbackstack/MainActivity.kt
+++ app/src/main/java/com/example/fragmentbackstack/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.fragmentbackstack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
@@ -22,5 +23,171 @@ class MainActivity : AppCompatActivity() {
         manager = supportFragmentManager
 
     }
+
+    private fun addA(){
+
+        val a = FragmentA()
+
+        val transaction = manager.beginTransaction()
+
+        transaction.add(R.id.group,a,"A")
+
+        transaction.addToBackStack("addA")
+
+        transaction.commit()
+
+
+    }
+
+    private fun removeA(){
+
+        val a = manager.findFragmentByTag("A")
+
+        val transaction = manager.beginTransaction()
+
+        if(a != null){
+
+            transaction.remove(a)
+
+            transaction.addToBackStack("removeA")
+
+            transaction.commit()
+
+
+        }
+
+        else{
+
+            Toast.makeText(this,"The fragment A was not added before ",Toast.LENGTH_LONG).show()
+
+
+        }
+
+
+    }
+
+    private fun detachA(){
+
+        val a = manager.findFragmentByTag("A")
+
+        val transaction = manager.beginTransaction()
+
+        if(a != null){
+
+            transaction.detach(a)
+
+            transaction.addToBackStack("detachA")
+
+            transaction.commit()
+
+
+        }
+
+
+    }
+
+    private fun attachA(){
+
+        val a = manager.findFragmentByTag("A")
+
+        val transaction = manager.beginTransaction()
+
+        if(a != null){
+
+            transaction.attach(a)
+
+            transaction.addToBackStack("attachA")
+
+            transaction.commit()
+
+        }
+
+    }
+
+
+    private fun replacewithA(){
+
+        val a = FragmentA()
+
+        val transaction = manager.beginTransaction()
+
+        transaction.replace(R.id.group,a,"A")
+
+        transaction.addToBackStack("replacewithA")
+
+        transaction.commit()
+
+
+    }
+
+
+    private fun addB(){
+
+        val b = FragmentB()
+
+        val transaction = manager.beginTransaction()
+
+        transaction.add(R.id.group,b,"B")
+
+        transaction.addToBackStack("addB")
+
+        transaction.commit()
+
+    }
+
+    private fun removeB(){
+
+        val b = manager.findFragmentByTag("B")
+
+        val transaction = manager.beginTransaction()
+
+        if(b!= null){
+
+            transaction.remove(b)
+
+            transaction.addToBackStack("removeB")
+
+            transaction.commit()
+
+        }
+
+        else{
+            Toast.makeText(this,"The fragment B was not added before ",Toast.LENGTH_LONG).show()
+
+        }
+
+
+
+    }
+
+    private fun replacewithB(){
+
+        val b = FragmentB()
+
+        val transaction = manager.beginTransaction()
+
+        transaction.replace(R.id.group,b,"B")
+
+        transaction.addToBackStack("replacewithB")
+
+        transaction.commit()
+
+    }
+
+    private fun popaddB(){
+
+        manager.popBackStack("addB",0)
+
+
+    }
+
+
+    private fun back(){
+
+        manager.popBackStack()
+
+    }
+
+
 
 }
