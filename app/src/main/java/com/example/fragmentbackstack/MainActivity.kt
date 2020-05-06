fb10ca6fb76fcdd44af7ddfbeb7829171f5eeda3
goodideas <goodideas@goodideasde-MacBook-Pro.local>
2020-05-06 14:18:20 +0800

    setData

--- app/src/main/java/com/example/fragmentbackstack/MainActivity.kt
+++ app/src/main/java/com/example/fragmentbackstack/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.fragmentbackstack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var manager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setData ()
+
     }
+
+    fun setData (){
+
+        manager = supportFragmentManager
+
+    }
+
 }
